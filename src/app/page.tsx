a184c7ddcba42938a361f614794cdc52210e5142
ric <ric@localhost.localdomain>
2024-12-16 14:27:14 -0300

    1st

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Home() {
+
+
+  return (
+    
+    <>
+    
+      <div style={{border:"4px solid black", height: "100vh", width:"100%"}}>
+
+
+      </div>
+    
+    </>
+  
+  );
+
+}
